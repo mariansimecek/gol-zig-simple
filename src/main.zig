a02cadab8f51cbe9283bdb6b0d8774a6f1006439
Marian Šimeček <mariansimecek@gmail.com>
2024-06-09 20:49:46 +0200

    clean up

--- src/main.zig
+++ src/main.zig
@@ -10,7 +10,7 @@ pub fn main() !void {
 
     while (true) {
         game.step();
-        try game.terminalPrint();
+        try terminalPrint(&game);
         std.time.sleep(1_000_000_00);
     }
 }
@@ -25,29 +25,6 @@ const Game = struct {
         for (&self.grid) |*cell| {
             cell.* = rand.int(u1);
         }
-    }
-    fn terminalPrint(self: *Game) !void {
-        const stdout = std.io.getStdOut().writer();
-        const grid = &self.grid;
-        // clear terminal
-        stdout.print("\x1B[2J\x1B[H", .{}) catch {};
-        var print_buffer: [(WIDTH * 2 + 1) * HEIGHT * 8]u8 = undefined;
-        var print_buffer_len: usize = 0;
-
-        try stdout.print("Iter: {any}\n", .{self.iter});
-
-        for (0..HEIGHT) |y| {
-            for (0..WIDTH) |x| {
-                const ch: []const u8 = if (grid[x + (y * WIDTH)] == 1) "\x1b[7m  \x1b[0m" else "  ";
-                std.mem.copyForwards(u8, print_buffer[print_buffer_len..], ch);
-                print_buffer_len += ch.len;
-            }
-            std.mem.copyForwards(u8, print_buffer[print_buffer_len..], "\n");
-            print_buffer_len += 1;
-        }
-
-        const result = print_buffer[0..print_buffer_len];
-        try stdout.print("{s}", .{result});
     }
     fn step(self: *Game) void {
         const grid = &self.grid;
@@ -90,3 +67,27 @@ const Game = struct {
         @memcpy(grid, &buffer);
     }
 };
+
+pub fn terminalPrint(self: *Game) !void {
+    const stdout = std.io.getStdOut().writer();
+    const grid = &self.grid;
+    // clear terminal
+    stdout.print("\x1B[2J\x1B[H", .{}) catch {};
+    var print_buffer: [(WIDTH * 2 + 1) * HEIGHT * 8]u8 = undefined;
+    var print_buffer_len: usize = 0;
+
+    try stdout.print("Iter: {any}\n", .{self.iter});
+
+    for (0..HEIGHT) |y| {
+        for (0..WIDTH) |x| {
+            const ch: []const u8 = if (grid[x + (y * WIDTH)] == 1) "\x1b[7m  \x1b[0m" else "  ";
+            std.mem.copyForwards(u8, print_buffer[print_buffer_len..], ch);
+            print_buffer_len += ch.len;
+        }
+        std.mem.copyForwards(u8, print_buffer[print_buffer_len..], "\n");
+        print_buffer_len += 1;
+    }
+
+    const result = print_buffer[0..print_buffer_len];
+    try stdout.print("{s}", .{result});
+}
